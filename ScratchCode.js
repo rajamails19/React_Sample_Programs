730c1927be3d7fb00e036072b56c5aa029368c48
Sri <rajamails19@gmail.com>
2023-10-21 10:36:16 -0700

    Create ScratchCode.js

---
+++ ScratchCode.js
@@ -0,0 +1,24 @@
+import React, { useState, useCallback } from 'react';
+
+function App() {
+  const centerStyle = {
+    alignItems: 'center',
+    backgroundColor: 'orange',
+    display: 'flex',
+
+    flexDirection: 'column',
+    justifyContent: 'center',
+
+    minHeight: '100vh',
+  };
+
+   return (
+    <>
+      <div style={centerStyle}>
+        <h1> </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
